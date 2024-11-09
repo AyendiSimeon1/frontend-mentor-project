93371b7958b287f314781cc4296ea5ea5f0d5fa9
Ayendi <mrayendi1@gmail.com>
2024-11-09 22:33:49 +0100

    added validators for user auth

--- src/models/User.js
+++ src/models/User.js
@@ -18,5 +18,53 @@ const userSchema = new mongoose.Schema({
         type: String,
         requied: [true, 'Password is required'],
         minLength: [6, 'Password must be at least 8 characters'],
+        select: false
+    },
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+    lastLogin: Date,
+    loginAttempts: {
+        type: Number,
+        default: 0
+    },
+    timestamps: true
+});
+
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+
+    try {
+        const salt = await bcrypt.genSalt(12);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+
+    } catch (error) {
+        next(error);
     }
-})
+});
+
+userSchema.methods.comparePassword = async function(candidatePassord) {
+    try {
+        return await bcrypt.compare(candidatePassword, this.password);
+    } catch (error) {
+        throw new Error(error);
+    }
+};
+
+
+userSchema.methods.createPasswordResetToken = function () {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+    this.passwordResetToken = crypto
+        .hash('sa255')
+        .update(resetToken)
+        .digest('hex');
+
+        this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+        
+        return resetToken;
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
---
+++ src/validators/auth.validator.js
@@ -0,0 +1,64 @@
+const Joi = require('joi');
+
+const signupSchema = Joi.object({
+    email: Joi.string()
+        .email()
+        .required()
+        .messages({
+            'string.email': 'Please provide a valid email address',
+            'any.required': 'Email is required'
+        }),
+    password: Joi.string()
+        .min(8)
+        .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$'))
+        .required()
+        .messages({
+            'string.min': 'Password must be at least 6 characters long',
+            'string.pattern.base': 'Password must contain at least one uppercase letter,  one lowercase letter, one number and one special character',
+            'any.required': 'Password is required'
+        })
+});
+
+
+const resetPasswordSchema = Joi.object({
+    token: Joi.string()
+      .required()
+      .messages({
+        'any.required': 'Reset token is required'
+      }),
+      
+    password: Joi.string()
+      .min(8)
+      .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$'))
+      .required()
+      .messages({
+        'string.min': 'Password must be at least 8 characters long',
+        'string.pattern.base': 'Password must contain at least one uppercase letter, one lowercase letter, one number and one special character',
+        'any.required': 'Password is required'
+      }),
+      
+    confirmPassword: Joi.string()
+      .valid(Joi.ref('password'))
+      .required()
+      .messages({
+        'any.only': 'Passwords do not match',
+        'any.required': 'Password confirmation is required'
+      })
+  });
+  
+  const forgotPasswordSchema = Joi.object({
+    email: Joi.string()
+      .email()
+      .required()
+      .messages({
+        'string.email': 'Please provide a valid email address',
+        'any.required': 'Email is required'
+      })
+  });
+  
+  module.exports = {
+    signupSchema,
+    loginSchema,
+    resetPasswordSchema,
+    forgotPasswordSchema
+  };
