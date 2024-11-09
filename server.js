fa623bd02105cf807c5884544e46cc5436757c51
Ayendi <mrayendi1@gmail.com>
2024-11-09 21:53:16 +0100

    setup project configuration

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const { connectDatabase } = require('./config/database');
+const { errorHandler, notFoundHandler } = require('./middleware/errorHandlers');
+const { createLogger } = require('./config/logger');
+
+const logger = createLogger();
+
+const app = require('./app');
+
+
+app.use(helmet());
+app.use(cors());
+app.use(compression());
+
+
+app.use(morgan('combined', { stream: { write: message => logger.info(message.trim())}}));
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+const normalizePort = (val) => {
+    const port = parseInt(val, 10);
+    if(isNaN(port)) return val;
+    if (port >= 0) return port;
+    return false;
+};
+
+
+const port = normalizePort(process.env.PORT || '3000' );
+
+const server = appl.listen(port, () => {
+    logger.info(`Server is listening on port ${port} `)
+});
+
